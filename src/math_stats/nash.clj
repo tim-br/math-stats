c39cf580e69bd3831dfb683d53876f97f6264d22
tmtwd <t.williams.im@gmail.com>
2017-04-27 08:30:22 -0400

    more stuff

--- src/math_stats/nash.clj
+++ src/math_stats/nash.clj
@@ -1,4 +1,5 @@
-(ns math-stats.nash)
+(ns math-stats.nash
+  (:require [clojure.data.generators :as gen]))
 
 (defn foo [bar]
   (cond
@@ -20,16 +21,67 @@
 
 (def ascore (atom 0))
 
-(doseq [e (range 10000)]
-  (let [a (rand)
-        b (rand)]
-    (if (< a 0.2)
+(def a-nash 0.8)
+(def b-nash 0.35)
+(def a-stat (rand))
+(def b-stat (rand))
 
-      (do
-        (if (< b 0.5)
-          (swap! ascore #(+ %1 %2) (foo '(a2 b1)))
-          (swap! ascore #(+ %1 %2) (foo '(a2 b2)))))
-      (do
-        (if (< b 0.5)
-          (swap! ascore #(+ %1 %2) (foo '(a1 b1)))
-          (swap! ascore #(+ %1 %2) (foo '(a1 b2))))))))
+(doseq [x (range 101)]
+  (let [b-nash (#(* (bigdec %) (bigdec 0.01)) x)]
+     #_[gen/*rnd* (java.util.Random. 9385949482)]
+    (do
+      (reset! ascore 0)
+      (doseq [e (range 1000000)]
+        (let [a (gen/double)
+              b (gen/double)]
+          (if (< a a-nash)
+
+            (do
+              (if (< b b-nash)
+                (swap! ascore #(+ %1 %2) (foo '(a1 b1)))
+                (swap! ascore #(+ %1 %2) (foo '(a1 b2)))))
+            (do
+              (if (< b b-nash)
+                (swap! ascore #(+ %1 %2) (foo '(a2 b1)))
+                (swap! ascore #(+ %1 %2) (foo '(a2 b2))))))))
+      (println (str "a-nash " a-nash "-- b-nash " b-nash ": " @ascore)))))
+
+(binding [gen/*rnd* (java.util.Random. 928495999222244583)]
+  (do
+    (reset! ascore 0)
+    (doseq [e (range 10000)]
+      (let [a (gen/double)
+            b (gen/double)]
+        (if (< a a-nash)
+
+          (do
+            (if (< b b-nash)
+              (swap! ascore #(+ %1 %2) (foo '(a1 b1)))
+              (swap! ascore #(+ %1 %2) (foo '(a1 b2)))))
+          (do
+            (if (< b b-nash)
+              (swap! ascore #(+ %1 %2) (foo '(a2 b1)))
+              (swap! ascore #(+ %1 %2) (foo '(a2 b2))))))))
+    (println @ascore)))
+
+(doseq [y (range 11)]
+  (doseq [x (range 11)]
+    (let [b-nash (#(* (bigdec %) (bigdec 0.1)) x)
+          a-nash (#(* (bigdec %) (bigdec 0.1)) y)]
+      (binding [gen/*rnd* (java.util.Random. 437)]
+        (do
+          (reset! ascore 0)
+          (doseq [e (range 10000)]
+            (let [a (gen/double)
+                  b (gen/double)]
+              (if (< a a-nash)
+
+                (do
+                  (if (< b b-nash)
+                    (swap! ascore #(+ %1 %2) (foo '(a1 b1)))
+                    (swap! ascore #(+ %1 %2) (foo '(a1 b2)))))
+                (do
+                  (if (< b b-nash)
+                    (swap! ascore #(+ %1 %2) (foo '(a2 b1)))
+                    (swap! ascore #(+ %1 %2) (foo '(a2 b2))))))))
+          (println (str "a-nash " a-nash "-- b-nash " b-nash ": " @ascore)))))))
